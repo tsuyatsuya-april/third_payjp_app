b6dbcf0f9e5a1e755cb3ce79e806fe85c302374f
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-09-10 12:02:20 +0900

    ユーザー情報変更ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'items/index'
   devise_for :users
   root 'items#index'
+
+  resources :users, only: [:show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
